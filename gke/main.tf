f09252265bfcf9d19a50fba0db1db711147c1a32
Marcelo Gondim <hideman@gmail.com>
2023-03-02 22:45:58 -0300

    main gke file fix count

--- gke/main.tf
+++ gke/main.tf
@@ -9,18 +9,19 @@ resource "google_container_cluster" "gke" {
   name     = var.gke_name
   location = var.region
 
-  # Configurar os pools de nós
+  # Criar os pools de nós
   dynamic "node_pool" {
     for_each = var.node_pools
     content {
-      name               = node_pool.key
-      initial_node_count = node_pool.value.initial_node_count
+      name = node_pool.key
 
       node_config {
         machine_type = node_pool.value.machine_type
         disk_size_gb = node_pool.value.disk_size_gb
         preemptible  = node_pool.value.preemptible
       }
+
+      initial_node_count = node_pool.value.initial_node_count
     }
   }
 }
