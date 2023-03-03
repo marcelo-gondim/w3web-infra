84e3146f2f8ff315f86a71bed897f5f910e5b690
Marcelo Gondim <hideman@gmail.com>
2023-03-02 22:42:33 -0300

    main gke file

---
+++ gke/main.tf
@@ -0,0 +1,31 @@
+# Configurar o provider do Google Cloud
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Criar o cluster GKE
+resource "google_container_cluster" "gke" {
+  name     = var.gke_name
+  location = var.region
+
+  # Configurar os pools de nós
+  dynamic "node_pool" {
+    for_each = var.node_pools
+    content {
+      name               = node_pool.key
+      initial_node_count = node_pool.value.initial_node_count
+
+      node_config {
+        machine_type = node_pool.value.machine_type
+        disk_size_gb = node_pool.value.disk_size_gb
+        preemptible  = node_pool.value.preemptible
+      }
+    }
+  }
+}
+
+# Exportar o nome do cluster GKE para outros módulos
+output "name" {
+  value = google_container_cluster.gke.name
+}
